330396c22e3091d5c71597f4bdd40d6aee202f37
vivekkatial <vivekkatial@gmail.com>
2019-10-22 18:55:36 +1100

    Added experimental infrastructure to run jobs on HPC

--- bin/create_experiments.sh
+++ bin/create_experiments.sh
@@ -1,20 +1,60 @@
 #!/bin/bash
 
-# Make sure
-cd /data/cephfs/punim1074/
+# --------------------------------------------
+# This script implements the make functionality
+# of the ez-experimentr. It will produce all
+# configurations of the experiments defined in
+# the parameters_template.yml file of your 
+# experiment repository.
+#
+# The script requires you to have configured
+# your Github account on the cluster
+#
+# Please ensure you have set up SSH-free login
+# with a public key
+#
+# Author: Vivek Katial
+# --------------------------------------------
 
-# Take repository
-git_url="https://github.com/vivekkatial/aqc-three-sat-sim"
+# Remove tmp folder from previous run
+rm -rf tmp
 
-# Clone Repo from GIT
-git clone $git_url 
+# Put helpers in temp location
+mkdir tmp
+mkdir tmp/bin/
+mkdir tmp/config/
 
-# List files in cluster
-ls
+mv parse_yaml.sh tmp/bin/
+mv experiment_config.yml tmp/config/
 
-# Remove 
-rm -rf $repo
+# include parse_yaml function   
+. tmp/bin/parse_yaml.sh
 
-pwd
+# read experiment configuration
+eval $(parse_yaml tmp/config/experiment_config.yml)
+
+# Change directory to project URI
+cd $experiment_cluster_uri
+
+# Clone the experiments repository
+if [ ! -d "$experiment_repository" ] ; then
+	# If experiment repo not present then clone the repository
+    git clone $experiment_github_url $experiment_repository
+    cd $experiment_repository
+else
+	# If experiment repo already there then use most recent repo
+    cd "$experiment_repository"
+    git pull $experiment_github_url
+fi
+
+# Checking if SLURM script for building experiments present
+if [ ! -e bin/build_experiment_files.slurm ] ; then
+	echo "Could not locate build_experiment_files.slurm shell script"	
+else
+	echo "Building experiments"
+	sbatch bin/build_experiment_files.slurm
+fi
+
+
 
 exit
