7ced0e3cfa8c7a6f28afe481a302e02b7aeadc79
Vivek Katial <vivekkatial@gmail.com>
2023-02-27 23:24:22 +1100

    Feat: adding -

--- bin/start_mlflow.sh
+++ bin/start_mlflow.sh
@@ -7,5 +7,14 @@
 # --------------------------------------------
 
 export PORT=5000
-docker run -d -p $PORT:5000 -e DISABLE_AUTH=true --env-file ./env.list ez-experimentr/mlflow && \
-echo "Mlflow Tracking Server running on http://localhost:$PORT"
+export PORT_LOCAL=5555
+
+if [ $1 = "local" ] ; then 
+    echo "Local Build"
+    docker run -d -p $PORT_LOCAL:5000 -e DISABLE_AUTH=true --env-file ./env.list ez-experimentr/mlflow && \
+    echo "Mlflow Tracking Server running on http://localhost:$PORT_LOCAL"
+else
+    echo "DB BACKEND INSTANCE"
+    docker run -d -p $PORT:5000 -e DISABLE_AUTH=true --env-file ./env.list ez-experimentr/mlflow && \
+    echo "Mlflow Tracking Server running on http://localhost:$PORT"
+fi
