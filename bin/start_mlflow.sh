8b9e71e41254124f1c812be848726b6fb9766ed0
Vivek Katial <vivekkatial@gmail.com>
2023-02-28 08:53:29 +1100

    feat: cleanup ports on Dockerfiles

--- bin/start_mlflow.sh
+++ bin/start_mlflow.sh
@@ -7,11 +7,11 @@
 # --------------------------------------------
 
 export PORT=5000
-export PORT_LOCAL=5000
+export PORT_LOCAL=3000
 
 if [ $1 = "local" ] ; then 
     echo "Local Build"
-    docker run -d -p $PORT_LOCAL:5000 -e DISABLE_AUTH=true --env-file ./env.list ez-experimentr/local-mlflow && \
+    docker run -d -p $PORT_LOCAL:3000 -e DISABLE_AUTH=true --env-file ./env.list ez-experimentr/local-mlflow && \
     echo "Mlflow Tracking Server running on http://localhost:$PORT_LOCAL"
 else
     echo "DB BACKEND INSTANCE"
