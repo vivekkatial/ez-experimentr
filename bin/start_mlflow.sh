556d08eecdf9bc621c2b5379f00add76e1c03c4c
Vivek Katial <vivekkatial@gmail.com>
2023-12-24 17:10:46 +1100

    fix: Adding basic auth

--- bin/start_mlflow.sh
+++ bin/start_mlflow.sh
@@ -9,7 +9,7 @@
 export PORT=5000
 export PORT_LOCAL=3000
 
-if [ $1 = "local" ] ; then 
+if [ "$1" = "local" ] ; then 
     echo "Local Build"
     docker run -d -p $PORT_LOCAL:3000 -e DISABLE_AUTH=true --env-file ./env.list ez-experimentr/local-mlflow && \
     echo "Mlflow Tracking Server running on http://localhost:$PORT_LOCAL"
