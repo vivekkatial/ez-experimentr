7b8bafe457fead433ecf19fb3912744ceeeadc09
Vivek Katial <vivekkatial@gmail.com>
2023-12-24 17:14:15 +1100

    feat: changing to turn off auth disabled feature

--- bin/start_mlflow.sh
+++ bin/start_mlflow.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # --------------------------------------------
-# This script is Deploys the mlflow container
+# This script Deploys the MLflow container
 #
 # Author: Vivek Katial
 # --------------------------------------------
@@ -9,12 +9,12 @@
 export PORT=5000
 export PORT_LOCAL=3000
 
-if [ "$1" = "local" ] ; then 
+if [ "$1" = "local" ]; then 
     echo "Local Build"
-    docker run -d -p $PORT_LOCAL:3000 -e DISABLE_AUTH=true --env-file ./env.list ez-experimentr/local-mlflow && \
+    docker run -d -p $PORT_LOCAL:3000 --env-file ./env.list ez-experimentr/local-mlflow && \
     echo "Mlflow Tracking Server running on http://localhost:$PORT_LOCAL"
 else
     echo "DB BACKEND INSTANCE"
-    docker run -d -p $PORT:5000 -e DISABLE_AUTH=true --env-file ./env.list ez-experimentr/mlflow && \
+    docker run -d -p $PORT:5000 --env-file ./env.list ez-experimentr/mlflow && \
     echo "Mlflow Tracking Server running on http://localhost:$PORT"
 fi
